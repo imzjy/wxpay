1931c1d631db3b4f9e02b93d5d4c9c5ee140a21e
zjy <imjatsz@gmail.com>
2015-06-05 15:32:20 +0800

    add htto.go

---
+++ http.go
@@ -0,0 +1,37 @@
+package wxpay
+
+import (
+	"crypto/tls"
+	"io/ioutil"
+	"net/http"
+	"bytes"
+)
+
+
+
+func Trans() ([]byte, error) {
+
+	req, err := http.NewRequest("POST", "https://api.mch.weixin.qq.com/pay/unifiedorder", bytes.NewBuffer([]byte("")))
+	if err != nil {
+		return []byte(""), err
+	}
+	req.Header.Add("Content-type", "application/x-www-form-urlencoded;charset=UTF-8")
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: false},
+	}
+	client := &http.Client{Transport: tr}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return []byte(""), err
+	}
+
+	defer resp.Body.Close()
+	respData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []byte(""), err
+	}
+
+	return respData, nil
+}
